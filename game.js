53b5ee4efc7880242c7b584acc1610b8557716bd
r77tchan <101853757+r77tchan@users.noreply.github.com>
2024-10-05 15:10:23 +0900

    Add files via upload

--- game.js
+++ game.js
@@ -3,30 +3,56 @@ const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
 const tile = {
-  image: null,
-  a: [0, 1],
-  b: [1, 1],
-  c: [2, 1],
-  d: [3, 1],
-  e: [4, 1],
-  f: [5, 1],
-  g: [6, 1],
-  h: [7, 1],
-  i: [0, 2],
-  j: [1, 2],
-  k: [2, 2],
-  l: [3, 2],
-  n: [4, 2],
-  m: [5, 2],
-  o: [6, 2],
-  p: [7, 2]
+  image1: null,
+  image2: null,
+  gateImg: null,
+  gateNum: 0,
+  gateTime: 0,
+  point: [
+    {},                                                    // 0、空
+    {i:1,sx:32,sy:32,sw:16,sh:16,dx: 0,dy: 0,dw:16,dh:16}, // 1、木材
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {i:2,sx:12,sy:58,sw: 4,sh: 4,dx:12,dy:12,dw: 4,dh: 4}, // 10、左上枠線
+    {i:2,sx: 0,sy:42,sw:16,sh: 4,dx: 0,dy:12,dw:16,dh: 4}, // 11、上枠線
+    {i:2,sx: 0,sy:58,sw: 4,sh: 4,dx: 0,dy:12,dw: 4,dh: 4}, // 12、右上枠線
+    {i:2,sx: 0,sy:16,sw: 4,sh:16,dx: 0,dy: 0,dw: 4,dh:16}, // 13、右枠線
+    {i:2,sx: 0,sy:48,sw: 4,sh: 4,dx: 0,dy: 0,dw: 4,dh: 4}, // 14、右下枠線
+    {i:2,sx: 0,sy:32,sw:16,sh: 4,dx: 0,dy: 0,dw:16,dh: 4}, // 15、下枠線
+    {i:2,sx:12,sy:48,sw: 4,sh: 4,dx:12,dy: 0,dw: 4,dh: 4}, // 16、左下枠線
+    {i:2,sx:12,sy:16,sw: 4,sh:16,dx:12,dy: 0,dw: 4,dh:16}, // 17、左枠線
+    {i:1,sx:32,sy:32,sw:16,sh:16,dx: 0,dy: 0,dw:16,dh:16}, // 18、木材、後に上からゲート
+  ]
 };
 
 const tileMap = [
-  [tile.a, tile.b, tile.c, tile.d],
-  [tile.e, tile.f, tile.g, tile.h],
-  [tile.i, tile.j, tile.k, tile.l],
-  [tile.n, tile.m, tile.o, tile.p]
+  [ 1,11,11,11,11,11,11,11,11, 0, 0, 0,11,11,11,11,11,11,11,11,12],
+  [17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,13],
+  [17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,13],
+  [17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,13],
+  [17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,13],
+  [17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,13],
+  [17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,13],
+  [17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,13],
+  [17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,13],
+  [ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1,18, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,13],
+  [17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,13],
+  [17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,13],
+  [17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,13],
+  [17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,13],
+  [17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,13],
+  [17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,13],
+  [17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,13],
+  [16,15,15,15,15,15,15,15,15, 0, 0, 0,15,15,15,15,15,15,15,15,14],
 ];
 
 const map = {
@@ -50,6 +76,9 @@ const player = {
   height: 24,
   speed: 1,
   image: null,
+  auraImg: null,
+  auraNum: 0,
+  auraTime: 0,
   animationFrameX: 1,
   animationFrameY: 2,
   animationCounter: 0,
@@ -70,6 +99,8 @@ const keys = {
   l: false,
   k: false,
   j: false,
+  g: false,
+  h: false
 };
 
 // イベントリスナーの設定
@@ -89,19 +120,22 @@ document.addEventListener('keyup', (e) => {
 window.addEventListener('resize', resizeCanvas);
 
 function resizeCanvas() {
-  // ウィンドウサイズに基づいてキャンバスのサイズを設定し、8の倍数に丸める
+  // ウィンドウサイズに基づいてキャンバスのサイズを設定し、2の倍数に丸める
   canvas.width = Math.floor(window.innerWidth / 2) * 2;
   canvas.height = Math.floor(window.innerHeight / 2) * 2;
-  deadZone.width = canvas.width / 3;
-  deadZone.height = canvas.height / 3;
+  deadZone.width = canvas.width / 4;
+  deadZone.height = canvas.height / 4;
   deadZone.x = (canvas.width - deadZone.width) / 2;
   deadZone.y = (canvas.height - deadZone.height) / 2;
-  // プレイヤーが進んでる分マイナスする
-  map.x = (canvas.width / 2) - (16 / 2) - player.positionX + player.width / 2;
-  map.y = (canvas.height / 2) - (24 / 2) - player.positionY + player.height / 2;
-  // マップ処理後に更新
+
+  // プレイヤー描画位置は常に中心！canvas/2で中心座標、プレイヤーサイズ/2を引くとプレイヤーの中心がcanvasの中心へ
   player.drawX = (canvas.width / 2) - (16 / 2);
   player.drawY = (canvas.height / 2) - (24 / 2);
+
+  // マップ描画開始座標はcanvasの中心からプレイヤー座標を引いた位置
+  map.x = canvas.width / 2 - player.positionX;
+  map.y = canvas.height / 2 - player.positionY;
+
   draw();
 }
 
@@ -111,6 +145,19 @@ function update() {
   let isMovingRight = false;
   let isMovingLeft = false;
   let isMovingDown = false;
+  if (keys.g || keys.h) {
+    player.speed = 2;
+    player.auraTime++;
+    if (player.auraTime > 10) {
+      player.auraTime = 0;
+      player.auraNum++;
+      if (player.auraNum > 2) {
+        player.auraNum = 0;
+      }
+    }
+  } else {
+    player.speed = 1;
+  }
   if (keys.ArrowUp || keys.e || keys.i) {
     if (deadZone.y < player.drawY) {
       player.drawY -= player.speed;
@@ -198,23 +245,50 @@ function update() {
 }
 
 function drawMap() {
-  for (let row = 0; row < tileMap.length; row++) {
-    for (let col = 0; col < tileMap[row].length; col++) {
-      const selected = tileMap[row][col];
-      const SCALE = 16;
-      for (let k = 0; k < SCALE; k++) {
-        for (let l = 0; l < SCALE; l++) {
-          ctx.drawImage(tile.image, selected[0] * 16, selected[1] * 16, 16, 16, 16 * (col * SCALE + l) + map.x, 16 * (row * SCALE + k) + map.y, 16, 16);
+  let gateX;
+  let gateY;
+  for (let i = 0; i < tileMap.length; i++) {
+    for (let j = 0; j < tileMap[i].length; j++) {
+      let index = tileMap[i][j];
+      if (index != 0) {
+        ctx.drawImage(tile['image'+tile.point[index].i], tile.point[index].sx, tile.point[index].sy, tile.point[index].sw, tile.point[index].sh, (16 * j) + tile.point[index].dx + map.x, (16 * i) + tile.point[index].dy + map.y, tile.point[index].dw, tile.point[index].dh);
+        if (index == 18) {
+          gateX = 16 * j;
+          gateY = 16 * i;
         }
       }
     }
   }
+  // ワープゲート
+  tile.gateTime++;
+  if (tile.gateTime > 10) {
+    tile.gateTime = 0;
+    tile.gateNum++;
+    if (tile.gateNum > 3) {
+      tile.gateNum = 0;
+    }
+  }
+  ctx.drawImage(tile.gateImg, 28, tile.gateNum*32, 16, 32, gateX + map.x, gateY - 12 + map.y, 16, 32);
+  // マップ外文字
+  ctx.font = '50px Arial';
+  ctx.fillStyle = 'red';
+  ctx.fillText('gg.mh4.jp', map.x + 50, map.y - 100);
+  ctx.font = '30px Arial';
+  ctx.fillText('『Ctrl』 + 『+』で拡大推奨', map.x - 20, map.y - 20);
+  ctx.fillText('移動→ESDF、IJKL', map.x - 20, map.y + tileMap.length * 16 + 30);
+  ctx.fillText('加速→G、H', map.x - 20, map.y + tileMap.length * 16 + 90);
+  ctx.fillText('攻撃→V、B', map.x - 20, map.y + tileMap.length * 16 + 150);
 }
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawMap();
+  // プレイヤー
+  if (player.speed === 2) {
+    ctx.drawImage(player.auraImg, 24*player.auraNum, 192/*64*/, 24, 32, player.drawX-4, player.drawY-8, 24, 32);
+  }
   ctx.drawImage(player.image, player.animationFrameX * 24, player.animationFrameY * 32, 24, 32, player.drawX - 4, player.drawY - 8, 24, 32);
+
 
   // デッドゾーンを視覚的に描画（デバッグ用）
   ctx.strokeStyle = 'red';
@@ -232,6 +306,15 @@ function draw() {
     player.width,
     player.height
   )
+
+  
+
+  // ctx.font = '40px Verdana';
+  // ctx.fillStyle = 'blue';
+  // ctx.textAlign = 'center';
+  // ctx.textBaseline = 'middle';
+  // ctx.fillText('You are dead!', canvas.width / 2, canvas.height / 2);
+
 }
 
 function gameLoop() {
@@ -243,15 +326,27 @@ function gameLoop() {
 function init() {
   player.image = new Image();
   player.image.src = 'player.png';
-  tile.image = new Image();
-  tile.image.src = 'map.png';
+  player.auraImg = new Image();
+  player.auraImg.src = 'aura.png';
+  tile.image1 = new Image();
+  tile.image1.src = 'map.png';
+  tile.image2 = new Image();
+  tile.image2.src = 'frame.png';
+  tile.gateImg = new Image();
+  tile.gateImg.src = 'gate.png';
   resizeCanvas();
-  // 初期化時の座標が拡大率と合わない場合にぼやけ発生
+  // プレイヤー描画位置は常に中心！canvas/2で中心座標、プレイヤーサイズ/2を引くとプレイヤーの中心がcanvasの中心へ
   player.drawX = (canvas.width / 2) - (16 / 2);
   player.drawY = (canvas.height / 2) - (24 / 2);
+
+  // 現在21マス*16ピクセル / 2（真ん中）が初期位置、座標はプレイヤー左上の点になる
+  player.positionX = tileMap[0].length * 16 / 2;
+  player.positionY = tileMap.length * 16 / 2;
+
+  // マップ描画開始座標はcanvasの中心からプレイヤー座標を引いた位置
+  map.x = canvas.width / 2 - player.positionX;
+  map.y = canvas.height / 2 - player.positionY;
   
-  map.x = player.drawX + player.width / 2;
-  map.y = player.drawY + player.height / 2;
   gameLoop();
 }
 
